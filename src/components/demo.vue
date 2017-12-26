6b646c828902c189eb9bfcc613f233782eb2e9d4
shanglingyue <shanglingyue@utry.cn>
2017-12-26 14:05:25 +0800

    节点封装

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -13,16 +13,38 @@ export default {
       fontSize: 16,
       horizonSpace: 40,
       verticalSpace: 20,
-      data: {
-        name: '根节点',
-        id: '1',
-        children: [
-          {
-            name: '子节点1',
-            id: '1-1'
-          }
-        ]
-      }
+      rootPosition: {
+        x: 100,
+        y: 100
+      },
+      data: [
+        {
+          name: '根节点',
+          id: '1',
+          children: [
+            {
+              name: '子节点1',
+              id: '1-1',
+              children: [
+                {
+                  name: 'safasfdasf',
+                  id: '1-2'
+                }
+              ]
+            },
+            {
+              name: '子节点1',
+              id: '1-1',
+              children: [
+                {
+                  name: 'safasfdasf',
+                  id: '1-2'
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   },
   methods: {
@@ -41,6 +63,7 @@ export default {
       obj.y = y
       obj.width = width
       obj.height = height
+      obj.children = []
       return obj
     },
     /** @description 画一个文本框
@@ -66,41 +89,66 @@ export default {
      *  @augments to 截止节点
      */
     drawLine (fromNode, toNode) {
-      let path1 = `M${fromNode.x + fromNode.width} ${fromNode.y + fromNode.height / 2}L${toNode.x} ${toNode.y + fromNode.height / 2}`
-      let line = this.svgInstance.path(path1)
+      let path = `M${fromNode.x + fromNode.width} ${fromNode.y + fromNode.height / 2}L${toNode.x} ${toNode.y + fromNode.height / 2}`
+      let line = this.svgInstance.path(path)
       line.attr({
         stroke: '#000',
         strokeWidth: 1
       })
     },
-    /** @description 将数据封装成节点对象 */
-    // createNodes (data) {
-    //   let root = this.Node(data.name, 100, 100)
-    //   this.createNode(root)
-    //   if (data.children && data.children.length > 0) {
-    //     let len = data.children.length
-    //     if (len === 1) {
-    //       let node = this.Node(data.name, root.x + this.horizonSpace, root.y)
-    //     } else {
-
-    //     }
-    //   }
-    // },
+    /** @description 将数据封装成节点对象
+     *  @augments data 要展示的流程数据
+     *  @augments parentNode 父节点
+     */
+    transformData (data, parentNode) {
+      let _this = this
+      let root = null
+      if (parentNode === null) {
+        root = this.Node(data[0].name, this.rootPosition.x, this.rootPosition.x)
+        data = data[0].children
+        this.transformData(data, root)
+      } else {
+        root = parentNode
+        let childNodesX = root.x + root.width + this.horizonSpace
+        data.forEach(function (item, i) {
+          let node = _this.Node(item.name, childNodesX, root.y + 50 * i)
+          root.children.push(node)
+          if (item.children && item.children.length > 0) {
+            _this.transformData(item.children, node)
+          }
+        })
+      }
+      return root
+    },
+    /** @description 画树 */
+    drawFlowChart (root) {
+      let _this = this
+      this.drawNode(root)
+      if (root.children && root.children.length > 0) {
+        root.children.forEach(function (item) {
+          _this.drawLine(root, item)
+          _this.drawFlowChart(item)
+        })
+      }
+    },
     /** @description 画图
      */
     test () {
       this.svgInstance = Snap('#svg')
-      let root = this.Node('根节点', 100, 100)
-      let test1 = this.Node('子节点1', 200, 100)
-      let test2 = this.Node('子节点2', 200, 200)
-      let test3 = this.Node('子节点3', 200, 300)
-      this.drawNode(root)
-      this.drawNode(test1)
-      this.drawNode(test2)
-      this.drawNode(test3)
-      this.drawLine(root, test1)
-      this.drawLine(root, test2)
-      this.drawLine(root, test3)
+      let root = this.transformData(this.data, null)
+      console.log(root)
+      this.drawFlowChart(root)
+      // let root = this.Node('根节点', 100, 100)
+      // let test1 = this.Node('子节点1', 200, 100)
+      // let test2 = this.Node('子节点2', 200, 200)
+      // let test3 = this.Node('子节点3', 200, 300)
+      // this.drawNode(root)
+      // this.drawNode(test1)
+      // this.drawNode(test2)
+      // this.drawNode(test3)
+      // this.drawLine(root, test1)
+      // this.drawLine(root, test2)
+      // this.drawLine(root, test3)
     }
   },
   mounted () {
