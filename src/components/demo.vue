b62ddac59be250fba54e4964373f4ba8c8bd86ed
shanglingyue <shanglingyue@utry.cn>
2017-12-26 15:55:30 +0800

    添加节点点击事件

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-    <button class="show" @click="test">添加</button>
+    <button class="show" @click="addNode">添加</button>
 		<svg id="svg" class="demo"></svg>
 	</div>
 </template>
@@ -17,6 +17,7 @@ export default {
         x: 100,
         y: 100
       },
+      selectedNode: {},
       data: [
         {
           name: '根节点',
@@ -26,6 +27,14 @@ export default {
               name: '子节点1',
               id: '1-1',
               children: [
+                {
+                  name: 'safasfdasf',
+                  id: '1-2'
+                },
+                {
+                  name: 'safasfdasf',
+                  id: '1-2'
+                },
                 {
                   name: 'safasfdasf',
                   id: '1-2'
@@ -50,15 +59,17 @@ export default {
   methods: {
     /** @description 将每一个节点封装成SVG中的节点，带有左上角坐标
      *  @augments value 节点文本内容
+     *  @augments id 节点id
      *  @augments x 节点左上角x坐标
      *  @augments y 节点左上角y坐标
      *  @returns 返回节点对象，包括节点内容，节点左上角坐标和节点的宽度和高度
     */
-    Node (value, x, y) {
+    Node (value, id, x, y) {
       let obj = {}
       let width = value.length * this.fontSize + 20
       let height = this.fontSize + 10
       obj.value = value
+      obj.id = id
       obj.x = x
       obj.y = y
       obj.width = width
@@ -71,9 +82,10 @@ export default {
      *  @returns 返回文本和框线的组合对象
      */
     drawNode (node) {
+      let _this = this
       let rect = this.svgInstance.rect(node.x, node.y, node.width, node.height, 5)
       rect.attr({
-        fill: '#bada55',
+        fill: '#fff',
         stroke: '#000',
         strokeWidth: 1
       })
@@ -82,6 +94,28 @@ export default {
       })
       let g = this.svgInstance.g()
       g.add(rect, text)
+      g.addClass('node ' + node.id)
+      g.click(function (e) {
+        Snap.selectAll('.node').forEach(function (item) {
+          item.children()[0].attr({
+            fill: '#fff',
+            stroke: '#000',
+            strokeWidth: 1
+          })
+        })
+        rect.attr({
+          fill: '#bada55',
+          stroke: '#000',
+          strokeWidth: 1
+        })
+        _this.selectedNode = node
+        console.log(_this.selectedNode)
+      })
+      g.mousedown(function (e) {
+        if (e.button === 2) {
+          alert(node.value)
+        }
+      })
       return node
     },
     /** @description 画两个节点之间的线
@@ -104,14 +138,16 @@ export default {
       let _this = this
       let root = null
       if (parentNode === null) {
-        root = this.Node(data[0].name, this.rootPosition.x, this.rootPosition.x)
+        root = this.Node(data[0].name, data[0].id, this.rootPosition.x, this.rootPosition.x)
         data = data[0].children
         this.transformData(data, root)
       } else {
         root = parentNode
         let childNodesX = root.x + root.width + this.horizonSpace
+        let len = data.length
+        let baseY = (2 * root.y - (len - 1) * (root.height + this.verticalSpace)) / 2
         data.forEach(function (item, i) {
-          let node = _this.Node(item.name, childNodesX, root.y + 50 * i)
+          let node = _this.Node(item.name, item.id, childNodesX, baseY + i * (root.height + _this.verticalSpace))
           root.children.push(node)
           if (item.children && item.children.length > 0) {
             _this.transformData(item.children, node)
@@ -131,24 +167,16 @@ export default {
         })
       }
     },
-    /** @description 画图
+    /** @description init
      */
     test () {
       this.svgInstance = Snap('#svg')
       let root = this.transformData(this.data, null)
-      console.log(root)
       this.drawFlowChart(root)
-      // let root = this.Node('根节点', 100, 100)
-      // let test1 = this.Node('子节点1', 200, 100)
-      // let test2 = this.Node('子节点2', 200, 200)
-      // let test3 = this.Node('子节点3', 200, 300)
-      // this.drawNode(root)
-      // this.drawNode(test1)
-      // this.drawNode(test2)
-      // this.drawNode(test3)
-      // this.drawLine(root, test1)
-      // this.drawLine(root, test2)
-      // this.drawLine(root, test3)
+    },
+    /** @description 添加子节点 */
+    addNode () {
+
     }
   },
   mounted () {
@@ -162,4 +190,7 @@ export default {
 		height:500px;
     border: 1px solid #ccc;
 	}
+  .node{
+    cursor: pointer;
+  }
 </style>
