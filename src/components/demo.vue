83545d406601ce95db7fcad5e9a0f3cf0d940960
shanglingyue <shanglingyue@utry.cn>
2017-12-27 14:58:37 +0800

    给树节点设置叶子节点的个数完成

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -10,11 +10,11 @@ export default {
     return {
       svgInstance: {},
       fontSize: 16,
-      horizonSpace: 40,
+      horizonSpace: 60,
       verticalSpace: 20,
       rootPosition: {
         x: 100,
-        y: 100
+        y: 300
       },
       selectedNode: {},
       data: [
@@ -27,21 +27,32 @@ export default {
               id: '1-1',
               children: [
                 {
-                  name: 'safasfdasf',
-                  id: '1-2'
-                },
-                {
-                  name: 'safasfdasf',
-                  id: '1-2'
-                },
-                {
-                  name: 'safasfdasf',
-                  id: '1-2'
+                  name: '子节点1-1',
+                  children: [
+                    {
+                      name: '子节点1-1-1'
+                    },
+                    {
+                      name: '子节点1-1-2'
+                    }
+                  ]
                 }
               ]
             },
             {
-              name: '这是一个有很长名字的节点',
+              name: '子节点2',
+              id: '1-1',
+              children: [
+                {
+                  name: '子节点2-1'
+                },
+                {
+                  name: '子节点2-2'
+                }
+              ]
+            },
+            {
+              name: '子节点3',
               id: '1-1'
             }
           ]
@@ -71,19 +82,20 @@ export default {
       obj.children = []
       return obj
     },
-    /** @description 获取节点中子节点的个数 */
-    getLeafCount (data, sum) {
+    /** @description 获取节点中叶子节点的个数 */
+    getLeafCount (data) {
+      let temp = 0
       data.forEach(item => {
-        if (item.children && item.children.length > 0) {
-          let temp = this.getLeafCount(item.children, sum)
-          sum += (temp === 0 ? 1 : temp)
-          this.$set(item, 'leafCount', sum)
-        } else {
+        if (!item.children || item.children.length === 0) {
           this.$set(item, 'leafCount', 0)
-          sum += 1
+          temp += 1
+        } else {
+          let subNodeLeaf = this.getLeafCount(item.children)
+          this.$set(item, 'leafCount', subNodeLeaf)
+          temp += subNodeLeaf
         }
       })
-      return sum
+      return temp
     },
     /** @description 获取字符串字节数，一个汉字两个字节 */
     getStrBytes (str) {
@@ -136,27 +148,34 @@ export default {
         strokeWidth: 1
       })
     },
-    /** @description 将数据封装成节点对象
+    /** @description 将数据封装成节点对象,并赋予节点位置信息
      *  @augments data 要展示的流程数据
      *  @augments parentNode 父节点
+     *  @augments preCount 该节点前面有多少个叶子节点
      */
-    transformData (data, parentNode) {
+    transformData (data, parentNode, preCount) {
       let _this = this
       let root = null
       if (parentNode === null) {
-        root = this.Node(data[0].name, data[0].id, data[0].leafCount, this.rootPosition.x, this.rootPosition.x)
+        root = this.Node(data[0].name, data[0].id, data[0].leafCount, this.rootPosition.x, this.rootPosition.y)
         data = data[0].children
-        this.transformData(data, root)
+        this.transformData(data, root, 0)
       } else {
         root = parentNode
         let childNodesX = root.x + root.width + this.horizonSpace
-        let len = data.length
-        let baseY = (2 * root.y - (len - 1) * (root.height + this.verticalSpace)) / 2
+        let baseY = root.y - (root.leafCount - 1) * (root.height + this.verticalSpace) / 2
         data.forEach(function (item, i) {
-          let node = _this.Node(item.name, item.id, data[0].leafCount, childNodesX, baseY + i * (root.height + _this.verticalSpace))
+          let PosY = 0
+          if (item.leafCount === 0) {
+            PosY = baseY + i * (root.height + _this.verticalSpace)
+          } else {
+            PosY = baseY + (item.leafCount - 1 + 2 * preCount) * (root.height + _this.verticalSpace) / 2
+          }
+          let node = _this.Node(item.name, item.id, item.leafCount, childNodesX, PosY)
           root.children.push(node)
+          preCount += item.leafCount
           if (item.children && item.children.length > 0) {
-            _this.transformData(item.children, node)
+            _this.transformData(item.children, node, preCount)
           }
         })
       }
@@ -177,8 +196,8 @@ export default {
      */
     init () {
       this.svgInstance = Snap('#svg')
-      this.getLeafCount(this.data, 0)
-      let root = this.transformData(this.data, null)
+      this.getLeafCount(this.data)
+      let root = this.transformData(this.data, null, 0)
       this.drawFlowChart(root)
     }
   },
@@ -190,7 +209,7 @@ export default {
 <style>
 	.demo {
 		width:800px;
-		height:500px;
+		height:1000px;
 	}
   .node{
     cursor: pointer;
